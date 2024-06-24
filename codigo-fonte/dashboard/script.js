0047f28ea034fcf2483aea2bbdba5237033dae5c
Lucas de Araújo <lucas.arl@protonmail.com>
2024-06-23 23:30:57 -0300

    Revisão Final
    Efetuado ajustes no styles.css.

--- codigo-fonte/dashboard/script.js
+++ codigo-fonte/dashboard/script.js
@@ -288,3 +288,23 @@ document.addEventListener('DOMContentLoaded', function () {
   sendEmailButton.addEventListener('click', sendEmail);
   sendWhatsAppButton.addEventListener('click', sendWhatsApp);
 });
+document.addEventListener('DOMContentLoaded', function () {
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".sidebarBtn");
+  sidebarBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+    if(sidebar.classList.contains("active")){
+      sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+    }else
+      sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+  }
+
+  document.getElementById("profile-icon").addEventListener("click", function() {
+    document.querySelector(".dropdown-menu").classList.toggle("show");
+  });
+
+  function setStatus(status) {
+    console.log("Status alterado para:", status);
+    document.querySelector(".dropdown-menu").classList.remove("show");
+  }
+});
